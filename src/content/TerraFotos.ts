da86d2d5322019517ebedf572c61e674c29d2d1a
LTasaycoQ <luistasayco3030@gmail.com>
2026-05-12 17:56:19 -0500

    actualizacion de terra-fotos

---
+++ src/content/TerraFotos.ts
@@ -0,0 +1,175 @@
+
+export interface TerraFotos {
+    images: string[],
+    alt?: string,
+    title?: string,
+    descripcion: string,
+}
+
+export const terraFotosData: TerraFotos[] = [
+  {
+ images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926175/R6M25442_qbs8cv.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926176/R6M25503_jkiers.webp"
+    ],    title: "Terra Andina Colonial Mansion",
+    descripcion: `
+    The Facade of Terra Andina Colonial Mansion stands as a magnificent example of Cusco’s viceregal architecture. 
+
+    The manor features solid stone walls beneath viceregal walls and lanterns; and traditional balconies, strategically located just half a block from the San Pedro Central Market, which boasts an iconic iron structure designed by Gustave Eiffel. 
+
+    The main entrance, guarded by an imposing wooden gate adorned with gilded bronze medallions, sits have a block from the historic San Pedro Church, creating an unparalleled architectural setting.
+    `,
+
+  },
+  {
+ images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926169/Lobby_texahf.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926180/Rest_1_bvkniv.webp"
+    ],    title: "Prueba card 1",
+    descripcion: "Estimulación de puntos reflejos en pies y manos que corresponden a órganos y sistemas del cuerpo.",
+
+  },
+  {
+ images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926175/R6M25442_qbs8cv.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1778193171/iStock-1702599131_zkt9ls.jpg"
+    ],    title: "Prueba card 1",
+    descripcion: "Estimulación de puntos reflejos en pies y manos que corresponden a órganos y sistemas del cuerpo.",
+
+  },
+  {
+  images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926169/Lobby_texahf.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926180/Rest_1_bvkniv.webp"
+    ],   title: "The Colonial Patio - Lobby",
+    descripcion: `
+    The elegance of colonial stone and wood converges in the front view. Majestic stone silhouettes guard the whispers of the 1700s, where time seems to have stood still.
+
+    Monuments that capture the essence of Colonial Art. Those colonial arches framed the landscape with the nostalgia of a golden era. 
+
+    Beneath the porticos, the sunset light played hide-and-seek among the worn stone columns. 
+
+    `,
+  },
+
+
+
+
+   {
+    images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926180/repcion_1_mcnh4a.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926181/Rest_3_ccdipv.webp"
+    ],    title: "Small Patio",
+    descripcion: `
+    Antes del restaurante. Comenzar el día en este salón es como sumergirse en el alma artística del Perú. Las paredes exhiben una colección de temas de la Escuela Cusqueña de Pintura, con delicados lienzos enmarcados en impresionantes marcos dorados.
+    Entre el resplandor celestial de los marcos dorados y los motivos coloniales, el bufé ofrece un festín de sabores en un ambiente donde la luz matutina hace brillar el arte, convirtiendo el desayuno en una experiencia estética inolvidable.  
+    `,
+  },
+  {
+    images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/Restaurant_whlqjb.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926181/Rest_3_ccdipv.webp"
+    ],    
+    title: "The Breakfast Buffet",
+    descripcion: `
+    El restaurante Terra, ubicado en la Mansión Colonial Terra Andina, conserva la esencia histórica y se distingue por sus magníficas construcciones coloniales del siglo XVIII.
+
+    Construidas con la antigua técnica de piedra seca, estas románticas columnas enmarcan un salón interior que inunda el espacio con una cálida luz.
+
+    El ambiente se ve realzado por mesas de travertino, elegantes sillas de hierro forjado y paredes adornadas con lienzos al estilo de la Escuela Cuzqueña, cuyos característicos marcos recuerdan al pan de oro, creando una continuidad artística y majestuosa en toda la planta principal.
+    `,
+  },
+  {
+ images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926195/Bar1_pnruco.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1778193171/iStock-1702599131_zkt9ls.jpg"
+    ],    title: "Bar",
+    descripcion: `
+    This bar is a journey through time where two worlds converge. To the right, the imposing solidity of stone walls pays tribute to pre-Hispanic legacy, while the colonial arches tell the story of Spanish influence. 
+
+    Facing this ancient heritage, sophistication takes center stage with mirrors framed in imitation gold leaf, creating an opulent atmosphere that balances raw history with contemporary luxury.
+
+    `,
+  },
+  {
+    images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/Salon-masajes_1_yaide0.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1778193171/iStock-1702599131_zkt9ls.jpg"
+    ], 
+    title: "Elysium Spa",
+    descripcion: `
+    This spa is a sanctuary of serenity hidden within the walls of a majestic colonial mansion. Modern design seamlessly intertwines with the original architecture, where state-of-the-art wellness technology coexists with the “Spanish Colonial era”. 
+    Featuring intelligent hydrotherapy circuits, minimalist treatment suites, and elite personalized service, every corner is crafted to transform well-being into an unforgettable experience that honors the past while embracing the future of relaxation. 
+
+    `,
+  },  
+  {
+ images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926175/R6M25442_qbs8cv.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1778193171/iStock-1702599131_zkt9ls.jpg"
+    ],    descripcion: `
+    Our Master King Suite redefines luxury with a spacious King bed and independent living areas.Designed for ultimate privacy, it features premium finishes and breathtaking views that invite absolute relaxation
+
+    The exquisite hand-carved woodwork, crafted with unparalleled artistry, blends seamlessly with contemporary luxury to create a space fit for royalty. 
+
+    Staying in the Master Suite is like living inside a work of art—a retreat where time stands still and the grandeur of the past embraces the most exclusive modern comforts. 
+
+    Detalles de Ultra-Lujo / Ultra-Luxury Details:Arte Sacro: Pinturas originales de la Escuela Cusqueña con marcos en pan de oro.Arquitectura: Arcos de piedra originales y carpintería artística tallada a mano.Ambiente: El espacio más amplio y privilegiado de la casona colonial.Experiencia: Diseñada para los viajeros más exigentes que buscan una inmersión histórica total. 
+    `,
+  },
+  {
+ images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926175/R6M25442_qbs8cv.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1778193171/iStock-1702599131_zkt9ls.jpg"
+    ],    descripcion: `
+    From this vantage point, the Master Suite reveals its finest feature: a dream closet functional yet contemporary in its design. 
+    `,
+  },
+  {
+ images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926175/R6M25442_qbs8cv.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1778193171/iStock-1702599131_zkt9ls.jpg"
+    ],    descripcion: `
+    Junior Suite King 
+    Experience the height of comfort in our Junior Suite Kings , which offers a spacious layout with a plush King bed and an inviting living area. 
+
+    `,
+  },
+  {
+ images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926175/R6M25442_qbs8cv.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1778193171/iStock-1702599131_zkt9ls.jpg"
+    ],    title: "Premium Room",
+    descripcion: `
+    with two Queen Size Beds 
+    Exclusivity in the Heart of the Mansion. Our Premium Rooms are the crown jewels of our 18th-century colonial estate. 
+
+    Strategically located around the majestic courtyard of the new extension, these rooms are an ode to modern art, standing out because they frame the 
+    featuring elegant and exquisite design that defines the space. As part of a limited and exclusive collection, they offer an unparalleled atmosphere of privacy and prestige. 
+
+    Here, the whispers of history meet modern luxury, allowing you to wake up surrounded by Cusco’s most authentic architecture in a setting that perfectly captures the soul of the 1700s. 
+
+    `,
+  },
+  {
+    images: [
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1770926175/R6M25442_qbs8cv.webp",
+      "https://res.cloudinary.com/dlgeap8h0/image/upload/v1778193171/iStock-1702599131_zkt9ls.jpg"
+    ],
+    title: "Classic Room",
+    descripcion: `
+    with Two Twin Size Bed.
+    A Sanctuary of History and Comfort Step back into the 18th century in our Classic Rooms, where Cusco’s colonial architecture meets contemporary elegance. 
+
+    Spanning these spaces are meticulously designed to provide a restful sanctuary after exploring the Imperial City. Each room features refined finishes and furnishings that evoke the grandeur of the past, seamlessly paired with modern essentials like a Smart TV and high-speed Wi-Fi. 
+
+    Relax in the warmth of independent heating and the softness of premium linens, ensuring a stay that is as cozy as it is unforgettable in the heart of historic Cusco.
+
+    `,
+  }
+
+
+
+]
+
+
